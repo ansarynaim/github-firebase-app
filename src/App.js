d39e904d5b9b05828beb57ab1e3b48a51a69861f
Naim <naimansary92@gmail.com>
2021-12-28 23:01:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+// react-router
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+//toast
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+//firebase
+
+import firebase from "firebase/app";
+import "firebase/auth";
+
+//components
+
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import PageNotFound from "./pages/pageNotFound"
+import { UserContext } from "./context/UserContext";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import firebaseConfig from "./config/firebaseConfig";
+
+// init firebase 
+
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+
+ const [user,setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer/>
+      <UserContext.Provider value={{user,setUser}}>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAefv6tKqUVuMlNVeqla8-KCL5NjBaU_Ig",
+  authDomain: "firbasegit.firebaseapp.com",
+  projectId: "firbasegit",
+  storageBucket: "firbasegit.appspot.com",
+  messagingSenderId: "71941425158",
+  appId: "1:71941425158:web:80675bc41445783e311b87",
+  measurementId: "G-HZ8MT8WBZZ",
+};
